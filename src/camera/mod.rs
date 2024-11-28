b3f56b400cc994ec294952566619b13e38c2ac8a
Tin Lai <tin@tinyiu.com>
2024-11-28 10:28:06 +1000

    feature guard
    Signed-off-by: Tin Lai <tin@tinyiu.com>

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -4,6 +4,8 @@ pub(crate) mod main_camera;
 pub(crate) mod window_camera;
 
 pub fn plugin(app: &mut App) {
-    app.add_plugins(window_camera::plugin)
-        .add_plugins(main_camera::plugin);
+    app
+        .add_plugins(main_camera::plugin)
+        .add_plugins(window_camera::plugin)
+    ;
 }
