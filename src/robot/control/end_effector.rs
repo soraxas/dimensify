96f648aed5a2f9d63fce5fbb488efb81f754efa1
Tin Lai <tin@tinyiu.com>
2024-12-19 13:21:01 +1000

    implement timer and expotential increasing parameters on ik
    Signed-off-by: Tin Lai <tin@tinyiu.com>

--- src/robot/control/end_effector.rs
+++ src/robot/control/end_effector.rs
@@ -1,11 +1,13 @@
-use bevy::prelude::*;
+use std::time::Duration;
+
+use bevy::{prelude::*, time::common_conditions::on_timer};
 use k::{InverseKinematicsSolver, JacobianIkSolver};
 
 use crate::{
     camera::window_camera::{build_camera_to_egui_img_texture, FloatingCamera},
     coordinate_system::prelude::*,
     robot::{control::DesireRobotState, RobotLink, RobotState},
-    util::math_trait_ext::BevyQuatDistanceTrait,
+    util::{expotential_iterator::ExponentialIterator, math_trait_ext::BevyQuatDistanceTrait},
 };
 use bevy_egui::EguiUserTextures;
 
@@ -13,7 +15,11 @@ pub fn plugin(app: &mut App) {
     app.register_type::<EndEffectorMode>()
         .register_type::<EndEffectorTarget>()
         .register_type::<EndEffectorUserMarker>()
-        .add_systems(Update, ee_target_to_target_joint_state)
+        .add_systems(
+            Update,
+            // the timer avoid too frequent updates to the ik target
+            ee_target_to_target_joint_state.run_if(on_timer(Duration::from_millis(150))),
+        )
         // .add_systems(Startup, spawn_user_ee_marker)
         .observe(insert_ee_target_by_name)
         .add_systems(Update, (draw_ee_absolute_marker, ee_absolute_marker_sync));
@@ -168,7 +174,7 @@ impl Default for EndEffectorTarget {
             rotation: None,
             translation_mode: EndEffectorMode::Absolute,
             rotation_mode: EndEffectorMode::Absolute,
-            allowable_target_distance: 0.1,
+            allowable_target_distance: 0.05,
             allowable_target_angle: 0.08,
         }
     }
@@ -184,6 +190,50 @@ pub struct EndEffectorTarget {
 
     pub allowable_target_distance: f32,
     pub allowable_target_angle: f32,
+}
+
+#[derive(Component, Debug, Reflect)]
+/// A struct that contains the parameters generating
+/// progressively larger values for the target distance and angle
+pub struct ProgressiveIkParameters {
+    /// If the distance is smaller than this value, it is reached.
+    pub allowable_target_distance_initial: f32,
+    pub allowable_target_distance_max: f32,
+    /// If the angle distance is smaller than this value, it is reached.
+    pub allowable_target_angle_initial: f32,
+    pub allowable_target_angle_max: f32,
+    pub max_steps: usize,
+}
+
+impl Default for ProgressiveIkParameters {
+    fn default() -> Self {
+        Self {
+            allowable_target_distance_initial: 0.001,
+            allowable_target_distance_max: 0.1,
+            allowable_target_angle_initial: 0.005,
+            // allowable_target_angle_max: 0.1,
+            allowable_target_angle_max: 0.4,
+            max_steps: 8,
+        }
+    }
+}
+
+impl ProgressiveIkParameters {
+    pub fn get_translation_iterator(&self) -> ExponentialIterator {
+        ExponentialIterator::new(
+            self.allowable_target_distance_initial,
+            self.allowable_target_distance_max,
+            self.max_steps,
+        )
+    }
+
+    pub fn get_rotation_iterator(&self) -> ExponentialIterator {
+        ExponentialIterator::new(
+            self.allowable_target_angle_initial,
+            self.allowable_target_angle_max,
+            self.max_steps,
+        )
+    }
 }
 
 impl EndEffectorTarget {
@@ -243,9 +293,15 @@ fn ee_target_to_target_joint_state(
         //     error!("{:#?} > {:#?}", n.link().clone().map(|l|l.name), "link");
         // }
 
-        let mut solver = JacobianIkSolver::default();
-        solver.allowable_target_distance = ee_target.allowable_target_distance;
-        solver.allowable_target_angle = ee_target.allowable_target_angle;
+        // let mut solver = JacobianIkSolver::new(
+        //     // default:
+        //     0.001,
+        //     0.005,
+        //     0.5,
+        //     35,
+        // );
+        // solver.allowable_target_distance = ee_target.allowable_target_distance;
+        // solver.allowable_target_angle = ee_target.allowable_target_angle;
 
         let constraints = k::Constraints {
             rotation_x: ee_target.rotation.is_some(),
@@ -342,28 +398,44 @@ fn ee_target_to_target_joint_state(
             // .swap_yz_axis_and_flip_hand()
             .flip_hand();
 
-        // even though arm is a reference, it is still mutable (due to the internal implementation)
-        match solver.solve_with_constraints(&arm, &target, &constraints) {
-            Ok(_) => {
-                // robot_state.set_changed();
-                // desire_robot_state.set_target(arm.joint_positions().to_vec());
+        let ik_progressive_params = ProgressiveIkParameters::default();
 
-                // let's do a final sanity check to see if the final result is true better than the previous one
-                let original_dist = ee_target.distance(arm_ee_transform);
-                let ik_solution_dist = ee_target.distance(arm.end_transform().to_bevy());
+        let translation_iter = ik_progressive_params.get_translation_iterator();
+        let rotation_iter = ik_progressive_params.get_rotation_iterator();
 
-                if ik_solution_dist < original_dist {
-                    dbg!("dist", original_dist, ik_solution_dist);
+        for (target_dist, target_rot) in translation_iter.zip(rotation_iter) {
+            let solver = JacobianIkSolver::new(
+                // default:
+                target_dist,
+                target_rot,
+                0.5,
+                35,
+            );
+
+            // even though arm is a reference, it is still mutable (due to the internal implementation)
+            match solver.solve_with_constraints(&arm, &target, &constraints) {
+                Ok(_) => {
+                    // robot_state.set_changed();
+                    // desire_robot_state.set_target(arm.joint_positions().to_vec());
+
+                    // let's do a final sanity check to see if the final result is true better than the previous one
+                    // let original_dist = ee_target.distance(arm_ee_transform);
+                    // let ik_solution_dist = ee_target.distance(arm.end_transform().to_bevy());
+
+                    // if ik_solution_dist < original_dist {
+                    // dbg!("dist", original_dist, ik_solution_dist);
 
                     // if the ik solution is better than the original one, we will update the robot state
                     commands.entity(entity).insert(DesireRobotState::new(
                         real_serial_link.unwrap(), // unwrap is safe here to get inner value
                         Some(arm.joint_positions().to_vec()),
                     ));
+                    break;
+                    // }
                 }
-            }
-            Err(err) => {
-                println!("Err: {err}");
+                Err(err) => {
+                    println!("Err: {err}");
+                }
             }
         }
     }
