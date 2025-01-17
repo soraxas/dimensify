0fcdaeb50a9e75f2b53460107d66c18c88265416
Tin Lai <tin@tinyiu.com>
2025-01-17 19:09:04 +1000

    add invert swap axis
    Signed-off-by: Tin Lai <tin@tinyiu.com>

--- src/coordinate_system/coordsys_transform.rs
+++ src/coordinate_system/coordsys_transform.rs
@@ -9,7 +9,7 @@ use bevy::prelude::Transform as BevyTransform;
 /// Trait for flipping the handiness of an object.
 pub trait SwapYZTrait {
     #[must_use]
-    fn swap_yz_axis(&self) -> Self;
+    fn swap_yz_axis(self) -> Self;
 }
 
 /// Trait for flipping the hand of an object.
@@ -29,7 +29,7 @@ pub trait SwapYZandFlipHandTrait {
 impl SwapYZTrait for BevyVec3 {
     #[inline(always)]
     #[must_use]
-    fn swap_yz_axis(&self) -> Self {
+    fn swap_yz_axis(self) -> Self {
         BevyVec3::new(self.x, self.z, self.y)
     }
 }
@@ -37,8 +37,28 @@ impl SwapYZTrait for BevyVec3 {
 impl SwapYZTrait for k::nalgebra::Translation3<f32> {
     #[inline(always)]
     #[must_use]
-    fn swap_yz_axis(&self) -> Self {
+    fn swap_yz_axis(self) -> Self {
         k::nalgebra::Translation3::new(self.x, self.z, self.y)
+    }
+}
+
+/////////////////////////
+
+pub trait SwapYZandFlipHandTraitInverse {
+    #[must_use]
+    fn swap_yz_axis_and_flip_hand_inverse(self) -> Self;
+}
+
+impl SwapYZandFlipHandTraitInverse for BevyTransform {
+    #[inline(always)]
+    #[must_use]
+    fn swap_yz_axis_and_flip_hand_inverse(self) -> Self {
+        self.mul_transform(BevyTransform::from_matrix(Mat4 {
+            x_axis: BevyVec4::new(1.0, 0.0, 0.0, 0.0), // X-axis remains the same
+            y_axis: BevyVec4::new(0.0, 0.0, -1.0, 0.0), // Y-axis becomes Z
+            z_axis: BevyVec4::new(0.0, 1.0, 0.0, 0.0), // Z-axis becomes Y
+            w_axis: BevyVec4::new(0.0, 0.0, 0.0, 1.0), // Homogeneous component
+        }))
     }
 }
 
@@ -85,8 +105,8 @@ impl SwapYZandFlipHandTrait for BevyTransform {
     fn swap_yz_axis_and_flip_hand(&self) -> Self {
         self.mul_transform(BevyTransform::from_matrix(Mat4 {
             x_axis: BevyVec4::new(1.0, 0.0, 0.0, 0.0), // X-axis remains the same
-            y_axis: BevyVec4::new(0.0, 0.0, 1.0, 0.0), // Y-axis becomes -Z
-            z_axis: BevyVec4::new(0.0, -1.0, 0.0, 0.0), // Z-axis becomes Y
+            y_axis: BevyVec4::new(0.0, 0.0, 1.0, 0.0), // Y-axis becomes Z
+            z_axis: BevyVec4::new(0.0, -1.0, 0.0, 0.0), // Z-axis becomes -Y
             w_axis: BevyVec4::new(0.0, 0.0, 0.0, 1.0), // Homogeneous component
         }))
     }
@@ -98,8 +118,8 @@ impl SwapYZandFlipHandTrait for k::nalgebra::UnitQuaternion<f32> {
     fn swap_yz_axis_and_flip_hand(&self) -> Self {
         self * k::nalgebra::UnitQuaternion::from_matrix(&k::nalgebra::Matrix3::new(
             1.0, 0.0, 0.0, // X-axis remains the same
-            0.0, 0.0, 1.0, // Y-axis becomes -Z
-            0.0, -1.0, 0.0, // Z-axis becomes Y
+            0.0, 0.0, 1.0, // Y-axis becomes Z
+            0.0, -1.0, 0.0, // Z-axis becomes -Y
         ))
     }
 }
@@ -113,8 +133,8 @@ impl SwapYZandFlipHandTrait for k::Isometry3<f32> {
             self.rotation.swap_yz_axis_and_flip_hand(),
             // k::nalgebra::UnitQuaternion::from_matrix(&k::nalgebra::Matrix3::new(
             //     1.0, 0.0, 0.0, // X-axis remains the same
-            //     0.0, 0.0, 1.0, // Y-axis becomes -Z
-            //     0.0, -1.0, 0.0, // Z-axis becomes Y
+            //     0.0, 0.0, 1.0, // Y-axis becomes Z
+            //     0.0, -1.0, 0.0, // Z-axis becomes -Y
             // )),
         )
     }
